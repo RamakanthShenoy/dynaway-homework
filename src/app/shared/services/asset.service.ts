1b48a6285e4258aab8be74c99928cd46afc1b0fe
Ramakanth Shenoy <RamakanthShenoy@users.noreply.github.com>
2024-04-20 23:09:51 +0530

    fix: Task 1: fix issue with assets data not loading and also add checks on response

--- src/app/shared/services/asset.service.ts
+++ src/app/shared/services/asset.service.ts
@@ -14,7 +14,9 @@ export class AssetService {
   }
 
   getAll(): Observable<Asset[]> {
-    return of(mockAssetHttpResponse).pipe(
+    //Check if the external response is having the desired objects and status and then only send the data back
+    // Else send an empty array
+    return of((mockAssetHttpResponse?.data && mockAssetHttpResponse.ok) ? mockAssetHttpResponse.data: []).pipe(
       delay(getRandomInt(1000) + 500), // fake random http delay,
       tap(() => { // a small chance for the data fetch to error
         if (getRandomInt(10) % 10 === 0) throw Error('Http error')
